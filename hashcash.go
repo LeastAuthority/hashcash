a192f16ab99e2867d16167c7faac91f50bc91d80
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2021-07-29 15:21:29 +0530

    add a randomized test for leadBits

---
+++ hashcash.go
@@ -0,0 +1,82 @@
+package hashcash
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha1"
+	"fmt"
+)
+
+type Stamp struct {
+	Version  int
+	Bits     uint
+	Date     string
+	Resource string
+	Rand     string
+	Counter  string
+}
+
+func (stamp Stamp) String() string {
+	return fmt.Sprintf("%d:%d:%s:%s::%s:%s", stamp.Version, stamp.Bits, stamp.Date, stamp.Resource, stamp.Rand, stamp.Counter)
+}
+
+func Mint(bits uint, resource string) (string, error) {
+	b := make([]byte, 12)
+	counter := 0
+	timestamp := "210623"
+	for true {
+		_, err := rand.Read(b)
+		if err != nil {
+			return "", err
+		}
+		attempt := Stamp{
+			Version:  1,
+			Bits:     bits,
+			Date:     timestamp,
+			Resource: resource,
+		}
+		if Valid(attempt.String(), bits) {
+			return attempt.String(), nil
+		}
+		counter += 1
+	}
+
+	return "", fmt.Errorf("could not mint a stamp for %d bits and resource \"%s\"", bits, resource)
+}
+
+func Valid(stamp string, bits uint) bool {
+	buffer := bytes.NewBufferString(stamp)
+	hash := sha1.New()
+	sha1sum := hash.Sum(buffer.Bytes())
+
+	return leadingBits(sha1sum, bits)
+}
+
+func leadingBits(shasum []byte, requiredBits uint) bool {
+	bits := uint(0)
+	for _, b := range shasum {
+		if bits >= requiredBits {
+			return true
+		}
+		if requiredBits-bits > 8 {
+			if b == 0 {
+				bits += 8
+			} else {
+				return false
+			}
+		} else {
+			mask := uint(1 << 7)
+			for i := 0; i < 8; i++ {
+				if (uint(b) & mask) != 0 {
+					return false
+				}
+				bits += 1
+				mask = mask >> 1
+				if bits >= requiredBits {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
---
+++ hashcash_test.go
@@ -0,0 +1,45 @@
+package hashcash
+
+import (
+	"reflect"
+	"testing"
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/gen"
+	"github.com/leanovate/gopter/prop"
+)
+
+func TestLeadingBits(t *testing.T) {
+	parameters := gopter.DefaultTestParameters()
+	parameters.Rng.Seed(1234) // to generate reproducible results
+
+	properties := gopter.NewProperties(nil)
+	properties.Property("random shasum byte array with a known number of leading zeros", prop.ForAll(
+		func(sha1sum []byte, n uint) bool {
+			// zero out leading n bits from randomly generated sha1sum
+			count := uint(0)
+			for i, _ := range sha1sum {
+				if count == n {
+					break
+				}
+
+				if n < 8 {
+					mask := uint8((1 << uint(8 - n)) - 1)
+					sha1sum[i] = sha1sum[i] & mask
+					break
+				} else {
+					sha1sum[i] = 0
+					count += 8
+				}
+			}
+			// TODO: turn on nth bit?
+			return leadingBits(sha1sum[:], n)
+		},
+		gen.SliceOfN(20, gen.UInt8Range(0,255),
+			reflect.TypeOf(uint8(0))).
+			SuchThat(func(v interface{}) bool {
+				return len(v.([]uint8)) > 0
+			}),
+		gen.UIntRange(0, 20*8),
+	))
+	properties.TestingRun(t)
+}
