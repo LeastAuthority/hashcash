013b6da32a798459c4353b585b9097214610f2de
Ramakrishnan Muthukrishnan <ram@leastauthority.com>
2021-08-03 16:00:49 +0530

    tests: use the new countLeadingZeros() function

--- hashcash_test.go
+++ hashcash_test.go
@@ -1,6 +1,7 @@
 package hashcash
 
 import (
+	"strings"
 	"testing"
 	"github.com/leanovate/gopter"
 	"github.com/leanovate/gopter/gen"
@@ -28,22 +29,6 @@ func fixedLeadingZeros(sha1sum []byte, n uint) {
 	}
 }
 
-func TestLeadingBits(t *testing.T) {
-	parameters := gopter.DefaultTestParameters()
-	parameters.Rng.Seed(1234) // to generate reproducible results
-
-	properties := gopter.NewProperties(nil)
-	properties.Property("random shasum byte array with a random number of leading zeros", prop.ForAll(
-		func(input []byte, bits uint) bool {
-			fixedLeadingZeros(input, bits)
-			return leadingBits(input, bits)
-		},
-		gen.SliceOfN(20, gen.UInt8Range(1,255)),
-		gen.UIntRange(0, 20*8),
-	))
-	properties.TestingRun(t)
-}
-
 func TestLeadingZeros(t *testing.T) {
 	parameters := gopter.DefaultTestParameters()
 	parameters.Rng.Seed(1234) // to generate reproducible results
@@ -61,3 +46,20 @@ func TestLeadingZeros(t *testing.T) {
 	properties.TestingRun(t)
 }
 
+func TestMint(t *testing.T) {
+	properties := gopter.NewProperties(nil)
+	properties.Property("generate random stamp and a random number of required zero bits and check validity", prop.ForAll(
+		func(bits uint, resource string) bool {
+			// mint a valid stamp and check the resulting
+			stamp, err := Mint(bits, resource)
+			if err != nil {
+				t.Errorf("minting unsuccessful: %v", err)
+			}
+
+			return len(strings.Split(stamp, ":")) == 7
+		},
+		gen.UIntRange(0, 20), // bits
+		gen.Const("give me a token"), // generate a printable string
+	))
+	properties.TestingRun(t)
+}
